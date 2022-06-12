03501f93772a75ff06edf204708184ba0d8623d1
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-06-12 08:14:27 +0300

    last draft

--- pages/components/Main.js
+++ pages/components/Main.js
@@ -1,43 +1,42 @@
 
-function Main() {
+function Main({ handleSubmit, cookies, result }) {
   return (
     <>
-    <div className="w-full bg-emerald-100 flex justify-center ">
-      <form className="rounded-md	my-10 w-3/5 py-5 px-5 bg-emerald-400 text-black">
-          <legend className="font-bold">
+    <div className="w-full bg-emerald-100 flex items-center content-center 
+    justify-center flex-col">
+      <form className="flex items-center justify-center flex-col rounded-md	
+      my-10 w-4/5 py-10 px-10 bg-emerald-400 text-black font-bold"
+      onSubmit={handleSubmit}>
+          <legend className="col-span-4 text-3xl my-3">
             Create Cookie Stand</legend>
 
-          <div>
-            <label>Location</label>
-            <input type={"text"} placeholder={"Bercelona"}/>
-          </div>
+            <div className="flex items-center justify-center my-2 w-full">
+              <label className="mx-2 my-2">Location</label>
+              <input name="loc" className="w-4/5 h-full mx-2 my-2 bg-gray-200 rounded-md 
+              shadow-md" 
+              type={"text"} placeholder={"Bercelona"}/>
+            </div>
 
-          <div>
-            <label>Minimum Customers per hour</label>
-            <input type={"number"} placeholder={"2"}/>
-          </div>
+            <div className="grid grid-cols-4 grid-rows-2 gap-1">
+              <label className="mx-2 mt-2">Minimum Customers per hour</label>
+              <label className="mx-2 mt-2">Maximum Customers per hour</label>
+              <label className="mx-2 mt-2">Average Cookies per sale</label>
+              
+              <button className="row-span-2 bg-emerald-700 w-full	
+              h-full">Create</button>  
 
-          <div>
-            <label>Maximum Customers per hour</label>
-            <input type={"number"} placeholder={"4"}/>
-          </div>
-
-          <div>
-            <label>Average Cookies per sale</label>
-            <input type={"text"} placeholder={"2.5"}/>
-          </div>
-
-          <button>
-            Create
-          </button>
+              <input name="min" className="mx-2 mb-2 h-fit" type={"number"} placeholder={"2"}/>
+              <input name="max" className="mx-2 mb-2 h-fit" type={"number"} placeholder={"4"}/>
+              <input name="avg" className="mx-2 mb-2 h-fit" type={"text"} placeholder={"2.5"}/>
+            </div>
       </form>
 
-      <p className="my-5 bg-emerald-100 text-black text-xl">
+      <p className="my-4 bg-emerald-100 text-black text-xl">
         Report table comming soon...</p>
       
-      <p className="my-5 bg-emerald-100 text-black text-xl">
-        Report </p>
-
+      <p className="my-4 bg-emerald-100 text-black text-xl">
+        {cookies} </p>
+ 
     </div>
     </>
   )
